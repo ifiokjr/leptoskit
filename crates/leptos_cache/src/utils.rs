f63965a2617aa38e285e8e8874851a5c39585cfe
Ifiok Jr. <ifiokotung@gmail.com>
2025-03-21 01:10:44 +0000

    refactor: remove `leptos_cache` since `leptos-fetch` is good enough

--- crates/leptos_cache/src/utils.rs
+++
@@ -1,6 +0,0 @@
-use std::sync::atomic::AtomicU64;
-
-pub(crate) fn random_u64_rolling() -> u64 {
-	static COUNTER: AtomicU64 = AtomicU64::new(0);
-	COUNTER.fetch_add(1, std::sync::atomic::Ordering::Relaxed)
-}
